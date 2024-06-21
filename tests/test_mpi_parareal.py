76c3c24e057d557000926e5ccdbe599e62f66ed9
oussama-floor9 <oussamabouhenniche14@gmail.com>
2024-06-21 11:43:04 +0200

    #91 fix and update test

--- tests/test_mpi_parareal.py
+++ tests/test_mpi_parareal.py
@@ -1,27 +1,31 @@
-from parareal.parareal.mpi_parareal import parareal
-from parareal.lorenz.lorenz import lorenz
+from parareal.parareal.mpi_parareal import mpi_parareal
 from parareal.solver.feuler import feuler
+from parareal.test_func import *
 from mpi4py import MPI
 import numpy as np
 
 
 def test_mpi_parareal():
-    # Model parameters
-    sigma, rho, beta = 10.0, 28.0, 8/3
-    U0 = np.array([1.0, 1.0, 1.0])
-    tspan = [0, 60]
-    lorenz_ = lambda t,state:lorenz(t, state, sigma, rho, beta)
+    Nh = 100 
+    tspan = [0, 4] 
+    y0 = [2] 
 
-    #Parareal params
-    N = 10000
-    tol = 1e-1
-    max_iter = 5
+    # tracons la solution exacte
+    t = np.linspace(0, 4, 100)
+    y = f_exacte(t)
+
+    tol = 1e-10
+    max_iter = 10
     G_Nh=1
-    F_Nh=100
-    G = lambda tspan,u0, :feuler(lorenz_, tspan, u0, G_Nh)[1][-1]
-    F = lambda tspan,u0, :feuler(lorenz_, tspan, u0, F_Nh)[1][-1]
+    F_Nh=10
+    G = lambda tspan,u0, :feuler(f, tspan, u0, G_Nh)[1][-1]
+    F = lambda tspan,u0, :feuler(f, tspan, u0, F_Nh)[1][-1]
 
     try:
-        parareal(G, F, tspan, U0, N, max_iter, tol)
+        t_sol, iterations, solution, time = mpi_parareal(G, F, tspan, y0, Nh, max_iter, tol)
+        y_exac =  np.reshape(f_exacte(t),np.array(solution[-1]).shape)
+        error = np.linalg.norm(solution[-1] - y_exac) / np.linalg.norm(y_exac)
+        assert error <= 1e-8
+        
     except:
         print("Error when executing parareal in Parallel function")
